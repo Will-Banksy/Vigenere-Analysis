d5c3de82dd46ad54766775d36d8fbf928525c315
William Banks <williamjb555@gmail.com>
2020-09-18 14:21:14 +0100

    Added view of ciphertext and increased number of columns in Kasiski table

--- src/main/MainFrame.java
+++ src/main/MainFrame.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GraphicsConfiguration;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -9,67 +10,17 @@ import java.awt.Insets;
 import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.table.AbstractTableModel;
+import javax.swing.JTextArea;
 import javax.swing.table.TableColumn;
 
 import main.AnalysisUtils.RepeatedSequence;
 
 public class MainFrame extends JFrame {
 	private static final long serialVersionUID = 2231850685721535100L;
-	
-	private class NonEditableTableModel extends AbstractTableModel {
-		private static final long serialVersionUID = 8926476692586848995L;
-		
-		private String[] columnNames;
-		private Object[][] data;
-
-		public NonEditableTableModel(Object[][] data, String[] columnNames) {
-			this.columnNames = columnNames;
-			this.data = data;
-		}
-
-		public int getColumnCount() {
-			return columnNames.length;
-		}
-
-		public int getRowCount() {
-			return data.length;
-		}
-
-		public String getColumnName(int col) {
-			return columnNames[col];
-		}
-
-		public Object getValueAt(int row, int col) {
-			return data[row][col];
-		}
-
-		public Class<?> getColumnClass(int c) {
-			return getValueAt(0, c).getClass();
-		}
-
-		/*
-		 * Don't need to implement this method unless your table's
-		 * editable.
-		 */
-		public boolean isCellEditable(int row, int col) {
-			//Note that the data/cell address is constant,
-			//no matter where the cell appears onscreen.
-			return false;
-		}
-
-		/*
-		 * Don't need to implement this method unless your table's
-		 * data can change.
-		 */
-		public void setValueAt(Object value, int row, int col) {
-			data[row][col] = value;
-			fireTableCellUpdated(row, col);
-		}
-	}
 	
 	public JPanel mainContent;
 
@@ -99,7 +50,11 @@ public class MainFrame extends JFrame {
 		
 		mainContent = new JPanel();
 		mainContent.setLayout(new GridBagLayout());
-		JScrollPane mainScrollArea = new JScrollPane(mainContent);
+		JPanel mainContentCont = new JPanel();
+		mainContentCont.setLayout(new BorderLayout());
+		mainContentCont.add(mainContent, BorderLayout.NORTH);
+		JScrollPane mainScrollArea = new JScrollPane(mainContentCont);
+		mainScrollArea.setAlignmentY(TOP_ALIGNMENT);
 		c.insets = new Insets(0, 5, 5, 5);
 		c.gridy = 1;
 		c.fill = GridBagConstraints.BOTH;
@@ -109,6 +64,7 @@ public class MainFrame extends JFrame {
 		setText.addActionListener((actionEvent) -> {
 			Dialogs.showInputAreaDialog(this, "Enter Ciphertext", (text, submitted) -> {
 				if(submitted) {
+					text = text.replaceAll("\\s+",""); // Remove all whitespaces
 					setupMainContent(text);
 				}
 				return true;
@@ -124,17 +80,37 @@ public class MainFrame extends JFrame {
 		mainContent.removeAll();
 		
 		GridBagConstraints c = new GridBagConstraints();
+		c.insets = new Insets(5, 5, 5, 5);
 		
-		JTable table = visualiseKasiskiData(ciphertext);
+		JLabel ciphertextAreaLabel = new JLabel("Cipher Text");
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.weightx = 1;
+		mainContent.add(ciphertextAreaLabel, c);
+		
+		JTextArea ciphertextArea = new JTextArea(ciphertext);
+		ciphertextArea.setEditable(false);
+		JScrollPane ciphertextAreaSPane = new JScrollPane(ciphertextArea);
+		ciphertextAreaSPane.setMaximumSize(new Dimension(Integer.MAX_VALUE, 100));
+		ciphertextAreaSPane.setMinimumSize(new Dimension(20, 100));
+		c.gridy = 1;
+		c.insets = new Insets(0, 5, 10, 5);
+		mainContent.add(ciphertextAreaSPane, c);
+		
+		JLabel kasiskiLabel = new JLabel("Kasiski Examination");
+		c.gridy = 2;
+		c.insets = new Insets(0, 5, 5, 5);
+		mainContent.add(kasiskiLabel, c);
+
+		JTable table = visualiseKasiskiData(ciphertext);
 		JPanel tableContainer = new JPanel();
 		tableContainer.setLayout(new BorderLayout());
 		tableContainer.add(table.getTableHeader(), BorderLayout.NORTH);
 		tableContainer.add(table, BorderLayout.CENTER); // Need to wrap it in a JScrollPane or it doesn't show column headers
+		c.gridy = 3;
 		mainContent.add(tableContainer, c);
 		
 		mainContent.revalidate();
+		getContentPane().repaint();
 		pack();
 		setLocationRelativeTo(null);
 	}
@@ -143,7 +119,7 @@ public class MainFrame extends JFrame {
 	public JTable visualiseKasiskiData(String ciphertext) {
 		ArrayList<RepeatedSequence> list = AnalysisUtils.KasiskiTest(ciphertext);
 		
-		int numCols = 20;
+		int numCols = 36;
 		
 		String[] columnNames = Utils.concat(new String[]{ "Sequence" }, Utils.count(2, numCols + 1));
 		
---
+++ src/main/NonEditableTableModel.java
@@ -0,0 +1,54 @@
+package main;
+
+import javax.swing.table.AbstractTableModel;
+
+public class NonEditableTableModel extends AbstractTableModel {
+	private static final long serialVersionUID = 8926476692586848995L;
+	
+	private String[] columnNames;
+	private Object[][] data;
+
+	public NonEditableTableModel(Object[][] data, String[] columnNames) {
+		this.columnNames = columnNames;
+		this.data = data;
+	}
+
+	public int getColumnCount() {
+		return columnNames.length;
+	}
+
+	public int getRowCount() {
+		return data.length;
+	}
+
+	public String getColumnName(int col) {
+		return columnNames[col];
+	}
+
+	public Object getValueAt(int row, int col) {
+		return data[row][col];
+	}
+
+	public Class<?> getColumnClass(int c) {
+		return getValueAt(0, c).getClass();
+	}
+
+	/*
+	 * Don't need to implement this method unless your table's
+	 * editable.
+	 */
+	public boolean isCellEditable(int row, int col) {
+		//Note that the data/cell address is constant,
+		//no matter where the cell appears onscreen.
+		return false;
+	}
+
+	/*
+	 * Don't need to implement this method unless your table's
+	 * data can change.
+	 */
+	public void setValueAt(Object value, int row, int col) {
+		data[row][col] = value;
+		fireTableCellUpdated(row, col);
+	}
+}
